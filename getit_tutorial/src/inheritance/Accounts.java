e5784b2b6d39a11818e95aec27f091159830c628
pravendrachauhan <praveen.07022016@gmail.com>
2017-07-09 22:27:08 +0530

    trying to push new code

--- getit_tutorial/src/inheritance/Accounts.java
+++ getit_tutorial/src/inheritance/Accounts.java
@@ -2,7 +2,7 @@ package inheritance;
 
 public class Accounts {
 //	public int salary=5000;
-	
+	//test github
 	public static void main(String[] args) {
 //		Accounts acObj=new Accounts();
 //		System.out.println("Salary of Accounts department: "+acObj.salary);
